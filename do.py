268f75692937636b53cb83c46a1ede91793790f5
jingfan Zheng <jingfan.zheng@postgrad.manchester.ac.uk>
2026-07-28 12:15:15 +0100

    change file address

--- do.py
+++ do.py
@@ -2,7 +2,7 @@ import csv
 
 
 if __name__ == "__main__":
-    data_path = "Data/Iris.csv"
+    data_path = "/Users/user/Test/Iris.csv"
 
     with open(data_path, 'r', newline='') as input_data:
         reader = csv.DictReader(input_data)
